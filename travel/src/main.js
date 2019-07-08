2d422773790f6f2f344c5208cb83d279c4a0a218
xdw <898618984@qq.com>
2019-07-08 22:00:02 +0800

    添加reset.css 和border.css，解决1px问题

--- travel/src/main.js
+++ travel/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import './assets/styles/reset.css'//默认css
+import './assets/styles/border.css'//解决1px像素的问题
 
 Vue.config.productionTip = false
 
