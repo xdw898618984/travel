f249401b7c0af27673e5dff870f4fd1814153ca3
xdw <898618984@qq.com>
2019-07-08 22:03:54 +0800

    下载fastclick 解决移动端点击300ms延迟的问题

--- travel/src/main.js
+++ travel/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import fastClick from 'fastclick'//解决移动端点击300ms延迟的问题
 import './assets/styles/reset.css'//默认css
 import './assets/styles/border.css'//解决1px像素的问题
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 new Vue({
   
   render: h => h(App),
