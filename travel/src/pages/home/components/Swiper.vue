e427a5efe2859d1ee35d9fb9bcad63c3f0cad14c
xdw <898618984@qq.com>
2019-07-09 22:18:48 +0800

    使用swiper的Loop 的环路轮播

--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -3,20 +3,10 @@
   <div class="wapper">
     <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide>
-        <img
-          class="swiper-img"
-          src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/57c0942b8ad742abf288489ee3acfcff.jpg_750x200_a23f426d.jpg"
-          alt
-        />
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.baseUrl" alt />
       </swiper-slide>
-      <swiper-slide>
-        <img
-          class="swiper-img"
-          src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/818f6cc784ae6669b74bbbb255414a53.jpg_750x200_66ca5873.jpg"
-          alt
-        />
-      </swiper-slide>
+
       <!-- Optional controls -->
       <div class="swiper-pagination" slot="pagination"></div>
       <!-- <div class="swiper-button-prev" slot="button-prev"></div> -->
@@ -31,22 +21,50 @@ export default {
   data() {
     return {
       swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        },
+        loop: true
         // some swiper options/callbacks
         // 所有的参数同 swiper 官方 api 参数
         // ...
-      }
+      },
+      swiperList: [
+        {
+          id: "001",
+          baseUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/a4c27b0a5fc1d9e9e620225869089b12.jpg_750x200_e4720428.jpg"
+        },
+        {
+          id: "002",
+          baseUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/818f6cc784ae6669b74bbbb255414a53.jpg_750x200_66ca5873.jpg"
+        },
+        {
+          id: "003",
+          baseUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/a5d26ebec399b3aef013f45771ae96f0.jpg_750x200_de947eef.jpg"
+        }
+
+      ]
     };
   }
 };
 </script>
 <style lang="stylus" scoped>
-.wapper 
-   overflow: hidden
-   width: 100%
-   height:0
-   padding-bottom :31.25%
-   background :#eee
-  .swiper-img 
-  width: 100%;
+.wapper>>>.swiper-pagination-bullet-active {
+  background: white;
+}
 
+.wapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 26.7%;
+  background: #eee;
+}
+
+.swiper-img {
+  width: 100%;
+}
 </style>
